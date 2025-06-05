6251b87f42d6c75b8b1547cd835afbe4d9de2dd1
Leela Srinivasan <leelasrinivasan07@gmail.com>
2025-06-05 11:41:33 -0400

    Create mne_to_parcellation.py

---
+++ mne_to_parcellation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 5 11:37:56 2025
+@author: Leela Srinivasan
+
+Map MNE virtual sensors in source estimate stc obj to parcellation type (eg. 'Schaefer2018_400Parcels_7Networks_order')
+"""
+
+import os
+import sys
+import mne
+import numpy as np
+
+pnum_subj=sys.argv[1]
+dspm_run_dir=sys.argv[2]
+mri_subj_dir=sys.argv[3]
+parcellation_type=sys.argv[4]
+
+
+for hemi in ['lh', 'rh']:
+    fname = os.path.join(dspm_run_dir, 'full-stcs-{}.stc'.format(hemi))
+    stc=mne.read_source_estimate(fname)
+    
+    #Read cortical parcellation labels from a FreeSurfer annotation file, morph labels
+    read_labels = mne.read_labels_from_annot('fsaverage', parc = parcellation_type, subjects_dir=mri_subj_dir)
+    subject_labels = mne.morph_labels(read_labels, subject_to = pnum_subj, subjects_dir = mri_subj_dir)
+
+    # Iterate over each parcel in subject_labels, finding vertices corresponding to the label vertices
+    parcel_vertex_mapping = {}
+    for parcel_num, label in enumerate(subject_labels):
+        idx = np.nonzero(np.isin(stc.vertices[0 if hemi == 'lh' else 1], label.vertices))[0] 
+        parcel_vertex_mapping[parcel_num] = [sensor + 2562 if hemi == 'rh' else sensor for sensor in idx]
+        
+    # Save hemispheric dictionary mapping to dSPM folder
+    np.save(os.path.join(dspm_run_dir, "{}_parcel_vertex_mapping.npy".format(hemi)), parcel_vertex_mapping)
